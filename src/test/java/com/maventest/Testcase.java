de48f527123b00cb01357a1339d6d6693390f06f
Goricipati <GANITHA3@ford.com>
2022-10-26 21:30:42 -0400

    commiting the changes

---
+++ src/test/java/com/maventest/Testcase.java
@@ -0,0 +1,21 @@
+package com.maventest;
+
+import org.junit.Test;
+
+public class Testcase {  // green clor means it is done and it is not left uncomplete
+	
+	  @Test
+	    
+	    public void Testcase1()
+	    {
+	        System.out.println("Executing Test case1");
+	    }
+	    
+	    @Test
+	    
+	    public void Testcase2()
+	    {
+	        System.out.println("Executing Test case2");
+	    }
+
+}
